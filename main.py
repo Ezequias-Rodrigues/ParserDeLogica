c763c8dc1bab0529331cb8ff9db95b5d93345819
Ezequias Rodrigues <7608708+Ezequias-Rodrigues@users.noreply.github.com>
2026-03-13 00:26:02 -0300

    Atualizando os comentários para serem comerantarios e não strdocs

--- main.py
+++ main.py
@@ -19,24 +19,24 @@ or_token = {}
 and_token = {} 
 
 def extract_all_parentheses(text):
-    '''
-    Não pretendo limitar a profundidade do nesting,
-    fui atrás de uma biblioteca que suporta recursão em regex, e encontrei a 'regex' que é uma extensão da biblioteca 're' do Python.
-    Ela permite usar a sintaxe (?R) para referenciar a expressão regular atual, o que é útil para lidar com estruturas nested como parênteses.
-    '''
+    
+    #Não pretendo limitar a profundidade do nesting,
+    #fui atrás de uma biblioteca que suporta recursão em regex, e encontrei a 'regex' que é uma extensão da biblioteca 're' do Python.
+    #Ela permite usar a sintaxe (?R) para referenciar a expressão regular atual, o que é útil para lidar com estruturas nested como parênteses.
+    
     result = []
     pattern = r'\((?:[^()]|(?R))*\)'
-    '''
-    Esse padrão vai ser para criar "tokens" de expressões lógicas que estejam delimitada por parênteses, mais a frente será criado uma maneira de lidar com as outras expressões
-    seguindo o a regra de precedência.
-    Explicação do padrão:
-    - `\\(` : corresponde a um parêntese de abertura literal.
-    - `(` ... `)` : define um grupo de captura para o conteúdo dentro dos parênteses. Até o regex tem que ser nested...
-    - `(?: ... )` : define um grupo de não captura para o conteúdo dentro dos parênteses. Isso é necessário para evitar que o regex capture cada nível de parênteses como um grupo separado.
-    - `[^()]` : corresponde a qualquer caractere que não seja um parêntese. Isso garante que o regex possa lidar com texto dentro dos parênteses sem se confundir com os parênteses de abertura e fechamento.
-    - `|` : operador "or" para alternar entre os caracteres que não são parênteses e a recursão.
-    - `(?R)` : refere-se à expressão regular atual, permitindo recursão para lidar com parênteses nested.
-     '''
+    
+    #Esse padrão vai ser para criar "tokens" de expressões lógicas que estejam delimitada por parênteses, mais a frente será criado uma maneira de lidar com as outras expressões
+    #seguindo o a regra de precedência.
+    #Explicação do padrão:
+    #- `\\(` : corresponde a um parêntese de abertura literal.
+    #- `(` ... `)` : define um grupo de captura para o conteúdo dentro dos parênteses. Até o regex tem que ser nested...
+    #- `(?: ... )` : define um grupo de não captura para o conteúdo dentro dos parênteses. Isso é necessário para evitar que o regex capture cada nível de parênteses como um grupo separado.
+    #- `[^()]` : corresponde a qualquer caractere que não seja um parêntese. Isso garante que o regex possa lidar com texto dentro dos parênteses sem se confundir com os parênteses de abertura e fechamento.
+    #- `|` : operador "or" para alternar entre os caracteres que não são parênteses e a recursão.
+    #- `(?R)` : refere-se à expressão regular atual, permitindo recursão para lidar com parênteses nested.
+     
     matches = regex.findall(pattern, text, regex.VERSION1)
     for match in matches:
         result.append(match)
@@ -45,9 +45,9 @@ def extract_all_parentheses(text):
     return result
 #Fora os parenteses, vou implementar a tokenização das expressões da de menor precedencia para a de maior, pq no caso dos parenteses, isso já é implicitamente resolvido
 def extract_op(text, op):
-    ''' Sei que é má prática MAAAAAAAAAAS acredito que dê para resolver isso usando splits ao invés de regex
-        Desde já, peço seu perdão
-    '''
+    #Sei que é má prática MAAAAAAAAAAS acredito que dê para resolver isso usando splits ao invés de regex
+    #   Desde já, peço seu perdão
+    #
     result = [text]
     matches = text.split(op,1)
     if(len(matches) > 1):
