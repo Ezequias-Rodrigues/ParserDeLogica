ca8722dc9275d5544a397b5cfbbeba5d701cc60c
Ezequias Rodrigues <7608708+Ezequias-Rodrigues@users.noreply.github.com>
2026-03-12 23:38:06 -0300

    extract_implication -> extract_op
    Como que eu não vi isso de primeira?

--- main.py
+++ main.py
@@ -40,19 +40,19 @@ def extract_all_parentheses(text):
         result.extend(inner)
     return result
 #Fora os parenteses, vou implementar a tokenização das expressões da de menor precedencia para a de maior, pq no caso dos parenteses, isso já é implicitamente resolvido
-def extract_implication(text):
-    ''' Sei que é má prática MAAAAAAAAAAS acredito que dê para resolver isso(e possivelmente os proximos operadores também) usando splits ao invés de regex
+def extract_op(text, op):
+    ''' Sei que é má prática MAAAAAAAAAAS acredito que dê para resolver isso usando splits ao invés de regex
         Desde já, peço seu perdão
     '''
     result = [text]
     matches = text.split(">",1)
     if(len(matches) > 1):
-        result.extend(extract_implication(matches[1]))
+        result.extend(extract_op(matches[1], op))
        
     return result
 text = " ((a.b)+c) (a.~(b>c)+d) ((~a+(b.(c+d))>e)>f) x.y>z i>j>k"
 text_implication = "i>j>k>l"
-all_matches = extract_implication(text_implication)
+all_matches = extract_op(text_implication, ">")
 for match in all_matches:
     #nests_token[match.count("(")] = match #No caso de expressões com parenteses, determinar o nível de nesting é simples, basta contar o número de parênteses de abertura.
     implication_token[match.count(">")] = match 
