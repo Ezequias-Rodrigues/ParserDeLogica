2dfa7b092894a80089c581bafbe52a56b2abf06a
Ezequias Rodrigues <7608708+Ezequias-Rodrigues@users.noreply.github.com>
2026-03-12 22:23:07 -0300

    tokenização de implicações

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import regex
+'''
+Simbolos que pretendo usar:
+- `.` para AND
+- `+` para OR
+- `>` para IMPLICAÇÃO
+- `~` para NEGACÃO
+- `(` e `)` para delimitar expressões, não são obrigatórios, mas ajudam a definir a precedência das operações.
+- TALVEZ, MUITO TALVEZ, eu implemente bicondicionais, mas vamos ver o quão difícil é lidar com as outras operações primeiro.
+'''
+
 token_count = 0
-tokens = {} 
+
+tokens = {}  #tokens[VALOR HEX TO token_count] = VALOR ORIGINAL
+nests_token = {} #nests_token[X] = [token1, token2, ...] onde X é representa o nivel de nesting que a expressão possui, e token1, token2, ... são os tokens que estão naquele nível de nesting.
+implication_token = {} #mesmo esquema mas com >
 def extract_all_parentheses(text):
     '''
     Não pretendo limitar a profundidade do nesting,
@@ -25,10 +38,24 @@ def extract_all_parentheses(text):
         result.append(match)
         inner = extract_all_parentheses(match[1:-1]) # Remove os parênteses externos antes de chamar recursivamente
         result.extend(inner)
-    
     return result
-
-text = " ((a.b)+c) (a.(b>c)+d) ((a+(b.(c+d))>e)>f)"
-all_matches = extract_all_parentheses(text)
+#Fora os parenteses, vou implementar a tokenização das expressões da de menor precedencia para a de maior, pq no caso dos parenteses, isso já é implicitamente resolvido
+def extract_implication(text):
+    ''' Sei que é má prática MAAAAAAAAAAS acredito que dê para resolver isso(e possivelmente os proximos operadores também) usando splits ao invés de regex
+        Desde já, peço seu perdão
+    '''
+    result = [text]
+    matches = text.split(">",1)
+    if(len(matches) > 1):
+        result.extend(extract_implication(matches[1]))
+       
+    return result
+text = " ((a.b)+c) (a.~(b>c)+d) ((~a+(b.(c+d))>e)>f) x.y>z i>j>k"
+text_implication = "i>j>k>l"
+all_matches = extract_implication(text_implication)
 for match in all_matches:
+    #nests_token[match.count("(")] = match #No caso de expressões com parenteses, determinar o nível de nesting é simples, basta contar o número de parênteses de abertura.
+    implication_token[match.count(">")] = match 
+    print(implication_token)
+   #print( nests_token)
     print(f"Match: {match}")
